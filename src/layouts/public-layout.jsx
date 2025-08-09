563d8d6db123837ba5a5ac05c36669ccfba43455
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-08-10 01:40:31 +0700

    fix loading public layout for header and footer

--- src/layouts/public-layout.jsx
+++ src/layouts/public-layout.jsx
@@ -2,8 +2,12 @@ import { Outlet } from "react-router-dom"
 import Header from "@/components/public/header"
 import Footer from "@/components/public/footer"
 import QuickContact from "@/components/public/quick-contact"
-
+import LoadingPage from "@/pages/common/loading-page";
+import { useSiteConfig } from "@/context/site-config-context";
 const PublicLayout = () => {
+   const { loading } = useSiteConfig();
+
+  if (loading) return <LoadingPage />;
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Header />
